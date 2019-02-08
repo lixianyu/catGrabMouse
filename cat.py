e8d9ddee050d6f5f5d1b56c55473ce9d89b922d0
lixianyu <lbonus@163.com>
2019-02-08 13:33:19 +0800

    添加注释。

--- cat.py
+++ cat.py
@@ -1,18 +1,7 @@
+#输入步进数，得到最后一轮被吃掉的老鼠的编号，以及第三轮被吃掉的老鼠的编号
+#一共有13只老鼠围成一个圆圈。
 
-magicNum = 13
-def eatThirteenthMouse(idx, mouses):
-    lens = len(mouses)
-    print('lens = ', lens)
-    theTargetMouse = idx + 13 - 1
-    if theTargetMouse > lens:
-        theTargetMouse = theTargetMouse - lens
-    print('theTargetMouse = ', theTargetMouse)
-    print('Cat ', mouses[theTargetMouse], "eatted!")
-    mouses.pop(theTargetMouse)
-    if theTargetMouse >= lens - 1:
-        return 0
-    return theTargetMouse
-
+magicNum = 13 #有13只老鼠，编号为1的老鼠的颜色为白色，其它为黑色
 gn = 1
 gtheThirdEatMouse = 0
 def eatThirteenthMouse1(idx, mouses):
@@ -26,7 +15,7 @@ def eatThirteenthMouse1(idx, mouses):
         m += 1
     theTargetMouse = idx
     # print('theTargetMouse = ', theTargetMouse)
-    print('Cat ', mouses[theTargetMouse], "eatted!")
+    print('Mouse ', mouses[theTargetMouse], "was eatted by my cat!")
     # print('gn = ',gn)
     if gn == 3:
         # print('haha')
@@ -59,5 +48,6 @@ while gn < mouseCount:
     # print(mouseList)
     gn += 1
     # print('\n')
+
 print('最后一个被吃的耗子是: ', mouseList[0])
 print('第三轮被吃掉的耗子是: ', gtheThirdEatMouse)
